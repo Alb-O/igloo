6dfb53b5da7bf7fe1328e4050c76c10525e53759
Home Manager User <albertoshea2@gmail.com>
2025-08-18 12:57:07 +1000

    feat: broot use correct global theme

--- modules/broot/default.nix
+++ modules/broot/default.nix
@@ -1,9 +1,21 @@
-{pkgs, ...}: {
+{ pkgs, globals, ... }:
+let
+  # Import the colorscheme system
+  colors = import ../../lib/themes globals;
+
+  # Generate the HJSON skin content with colorscheme variables
+  skinContent = import ./skins/dark-blue.nix { inherit colors; };
+in
+{
   home.packages = with pkgs; [
     broot
   ];
 
   xdg.configFile."broot/conf.hjson".source = ./conf.hjson;
+  xdg.configFile."broot/verbs.hjson".source = ./verbs.hjson;
+
+  # Generate skin file with colorscheme integration
+  xdg.configFile."broot/skins/dark-blue.hjson".text = skinContent;
 
   # Download and install the vscode font for broot icons
   home.file.".local/share/fonts/vscode.ttf".source = pkgs.fetchurl {
