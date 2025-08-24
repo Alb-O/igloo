9f8d46b76962bb45b22bf80ec1f3738928c0e126
Albert O'Shea <albertoshea2@gmail.com>
2025-08-24 13:19:01 +1000

    tweak: themed fzf tokyonight

--- home-manager/modules/fzf/default.nix
+++ home-manager/modules/fzf/default.nix
@@ -7,7 +7,11 @@
 }: let
   fzfOptions = [
     "--style=minimal"
-    "--border=none"
+    "--border=sharp"
+    "--color=fg:#c0caf5,bg:#1a1b26,hl:#7aa2f7"
+    "--color=fg+:#c0caf5,bg+:#292e42,hl+:#7dcfff"
+    "--color=info:#7aa2f7,prompt:#7dcfff,pointer:#7dcfff"
+    "--color=marker:#9ece6a,spinner:#9ece6a,header:#9ece6a"
     "--preview='file-preview {}'"
     "--tmux 90%,100%"
   ];
@@ -25,6 +29,8 @@ in {
     # Set FZF_DEFAULT_OPTS environment variable for shell integration
     home.sessionVariables = {
       FZF_DEFAULT_OPTS = lib.concatStringsSep " " fzfOptions;
+      FZF_DEFAULT_COMMAND = "rg --files";
+      FZF_CTRL_T_COMMAND = "rg --files";
     };
 
     # Write fzf configuration to profile sources directory for easy sourcing
@@ -32,6 +38,8 @@ in {
       # FZF configuration
       if command -v fzf >/dev/null 2>&1; then
         export FZF_DEFAULT_OPTS="${lib.concatStringsSep " " fzfOptions}"
+        export FZF_DEFAULT_COMMAND="rg --files"
+        export FZF_CTRL_T_COMMAND="rg --files"
       fi
     '';
   };
